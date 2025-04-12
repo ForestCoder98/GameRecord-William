ba45b055af354bbed093b6a989e5963d60d2b9d5
ForestCoder98 <williad98@gmail.com>
2025-04-12 23:05:09 +0200

    finished step 3

--- app.mjs
+++ app.mjs
@@ -5,3 +5,15 @@ const local_Game_History = "SavedGames";
 function saveToLocalStorage(game) {
     localStorage.setItem(local_Game_History, game);
 }
+
+function retrieveAllLocalGames() {
+    return localStorage.getItem(local_Game_History);
+}
+
+function outputJson(jsonString) {
+    return JSON.parse(jsonString)
+}
+
+function importJson(gameFile) {
+    saveToLocalStorage(JSON.stringify(gameFile));
+}
